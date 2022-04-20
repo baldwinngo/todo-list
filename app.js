4f6a9ede06b2ba08666717c0ef629a475016fa94
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-04-19 21:25:18 -0700

    Finished search bar functionality
    todo: styling

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const addTodoForm = document.querySelector('.todo-form');
 const newTodo = document.querySelector('#new-todo');
 const todoList = document.querySelector('.todo-list');
 const deleteTodo = document.querySelector('.delete-button');
+const search = document.querySelector('#search');
 
 addTodoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   todoList.innerHTML += `
     <li>
-      <p>${newTodo.value}</p>
+      <p class="todo-item">${newTodo.value}</p>
       <button class="delete-button">delete</button>
     </li>`;
   newTodo.value = '';
@@ -17,4 +18,15 @@ todoList.addEventListener('click', (e) => {
   if ((e.target.parentElement.tagName === 'LI') && (e.target.tagName === 'BUTTON')) {
     e.target.parentElement.remove();
   }
+})
+
+search.addEventListener('input', (e) => {
+  Array.from(document.querySelectorAll('.todo-item'))
+    .filter(test => {
+      if (!test.textContent.includes(e.target.value)) {
+        test.parentElement.style.display = 'none';
+      } else {
+        test.parentElement.style.display = '';
+      }
+    });
 })
